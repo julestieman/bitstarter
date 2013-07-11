ff18cf8c2c43da3e0f6a436dffbca33a5a48dd53
Ubuntu <ubuntu@ip-172-31-26-127.us-west-2.compute.internal>
2013-07-11 06:29:59 +0000

    updated to support full css and url

--- grader.js
+++ grader.js
@@ -19,12 +19,17 @@ References:
    - http://en.wikipedia.org/wiki/JSON
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+
+ + RESTLER
+   - https://github.com/danwrong/restler
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var URLFILE_DEFAULT = "http://morning-brushlands-5570.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -55,6 +60,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrlFile = function(urlfile, checksfile) {
+    $ = cheerio.load(urlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,11 +80,34 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        // .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        // .option('-u, --url <url>', 'url to index.html', URLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    /* originally all file all the time - need to determine which cmd line arg(s) passed in */
+    var checkJson;
+    var outJson;
+    if (program.file) {
+        /* same as before */
+        checkJson = checkHtmlFile(program.file, program.checks);
+        outJson = JSON.stringify(checkJson, null, 4);
+        console.log(outJson);
+    }
+    if (program.url) {
+        /* use restler to get url content */
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                console.log('Restler Error get url: ' + result.message);
+            } else {
+                console.log(program.url);
+                checkJson = checkUrlFile(program.url, program.checks);
+                outJson = JSON.stringify(checkJson, null, 4);
+                console.log(outJson);
+            }
+        });
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
